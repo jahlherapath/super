d75355cb111aed91b68be154f84326aefd4c0177
Jahl Herapath <jahl.herapath@gmail.com>
2020-04-09 09:16:52 +1000

    Background textures

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,11 +6,41 @@ import { Fragment } from "react"
 import Navigation from "../Navigation"
 import GlobalStyles from "../GlobalStyles"
 
+import BGLeft from "../../assets/bg-left.png"
+import BGRight from "../../assets/bg-right.png"
+
 export default function Layout({ children }) {
   return (
     <Fragment>
       <GlobalStyles />
       <Navigation />
+      <div
+        sx={{
+          position: "fixed",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          width: "50%",
+          backgroundImage: `url(${BGLeft})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      ></div>
+
+      <div
+        sx={{
+          position: "fixed",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          width: "50%",
+          backgroundImage: `url(${BGRight})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      ></div>
       <main
         sx={{
           minHeight: "100vh",
